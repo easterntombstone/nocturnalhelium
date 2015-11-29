cb5015c9183f7256631a9686b49872745b586e9f
afnrfcb7 <emory.merryman@gmail.com>
2015-11-29 20:51:37 +0000

    throws

--- src/digitalbird/easterntombstone/nocturnalhelium/core.clj
+++ src/digitalbird/easterntombstone/nocturnalhelium/core.clj
@@ -1,2 +1,2 @@
 (ns digitalbird.easterntombstone.nocturnalhelium.core)
-(defn mock-is [failure] (fn [predicate] (if (.equals failure predicate) false true)))
+(defn mock-is [failure exception] (fn [predicate] (if (.equals failure predicate) (throw exception) true)))

63599dd6a1002e11c21919f139154c353b7b9626
afnrfcb7 <emory.merryman@gmail.com>
2015-11-29 20:06:23 +0000

    switch

--- test/digitalbird/easterntombstone/nocturnalhelium/core_test.clj
+++ test/digitalbird/easterntombstone/nocturnalhelium/core_test.clj
@@ -1,4 +1,4 @@
 (ns digitalbird.easterntombstone.nocturnalhelium.core-test
   (:require [clojure.test :refer :all]
             [digitalbird.easterntombstone.nocturnalhelium.core :refer :all]))
-(deftest pass (testing "passes") (is (= true true)))
+(deftest is-test (testing "is") (is (= true true)))

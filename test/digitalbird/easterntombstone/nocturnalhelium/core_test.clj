49a7dc1458a9abfb92c26ba5c412eb2ccff7d01d
afnrfcb7 <emory.merryman@gmail.com>
2015-11-30 00:15:57 +0000

    added 2 tests

--- test/digitalbird/easterntombstone/nocturnalhelium/core_test.clj
+++ test/digitalbird/easterntombstone/nocturnalhelium/core_test.clj
@@ -1,5 +1,5 @@
 (ns digitalbird.easterntombstone.nocturnalhelium.core-test
   (:require [clojure.test :refer :all]
             [digitalbird.easterntombstone.nocturnalhelium.core :refer :all]))
-(deftest is-passing-test (testing "the happy path") (is ((mock-is "nothing") "something")))
-(deftest is-passing-failing (testing "the sad path") (is (= ((mock-is "something") "something") false)))
+(deftest mock-is-test-passing (testing "passes") ((mock-is nil) true))
+(deftest mock-is-test-failing (testing "fails") (is (thrown? RuntimeException ((mock-is (new RuntimeException)) false))))
